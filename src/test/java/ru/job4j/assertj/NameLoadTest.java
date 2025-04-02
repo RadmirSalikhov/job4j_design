196829660268952f1f1a810483c9ef96708827de
RadmirSalikhov <radmir.sf.02@gmail.com>
2025-04-02 15:06:41 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void whenParseNameWithoutKeyThenThrownException() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"key=value", "=value"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("=value")
+                .hasMessageContaining("a key");
+    }
+
+    @Test
+    void whenParseNameWithoutValueThenThrownException() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"key=", "key=value"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key=")
+                .hasMessageContaining("a value");
+    }
+
+    @Test
+    void whenParseNameWithoutEqualSignThenThrowException() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"key=value", "keyvalue"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("keyvalue")
+                .hasMessageContaining("the symbol '='");
+    }
+}
