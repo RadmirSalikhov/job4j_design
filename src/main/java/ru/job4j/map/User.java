076763060661648f28d128a825b7f1dabe94b1fc
RadmirSalikhov <radmir.sf.02@gmail.com>
2025-05-06 11:20:21 +0300

    5. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import java.util.Objects;
 
 public class User {
-    private String name;
-    private int children;
-    private Calendar birthday;
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -29,13 +29,16 @@ public class User {
     public static void main(String[] args) {
         Map<User, Object> map = new HashMap<>();
         Calendar birthday = Calendar.getInstance();
+        birthday.set(2015, Calendar.JANUARY, 1, 10, 0, 0);
+        birthday.set(Calendar.MILLISECOND, 123);
 
         User user1 = new User("John", 2, birthday);
+        User user2 = new User("John", 2, birthday);
+
         int hashCode1 = user1.hashCode();
         int hash1 = hashCode1 ^ (hashCode1 >>> 16);
         int bucket1 = hash1 & 15;
 
-        User user2 = new User("John", 2, birthday);
         int hashCode2 = user2.hashCode();
         int hash2 = hashCode2 ^ (hashCode2 >>> 16);
         int bucket2 = hash2 & 15;
@@ -43,8 +46,17 @@ public class User {
         map.put(user1, new Object());
         map.put(user2, new Object());
 
-        System.out.printf("user1 - хэшкод: %s, хэш: %s, бакет: %s", hashCode1, hash1, bucket1);
-        System.out.println();
-        System.out.printf("user2 - хэшкод: %s, хэш: %s, бакет: %s", hashCode2, hash2, bucket2);
+        System.out.printf("user1 - хэшкод: %s, хэш: %s, бакет: %s%n", hashCode1, hash1, bucket1);
+        System.out.printf("user2 - хэшкод: %s, хэш: %s, бакет: %s%n", hashCode2, hash2, bucket2);
+
+        System.out.println("Содержимое карты:");
+        for (Map.Entry<User, Object> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User{name='%s', children=%d, birthday=%tc}", name, children, birthday);
     }
 }
